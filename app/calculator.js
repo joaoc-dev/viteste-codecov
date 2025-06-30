fef4f9aae322916716b6f2d0bd43f68e4a201504
joaoc-dev <contact@joao-carvalho.dev>
2025-06-30 16:39:52 +0100

    feat: add isLeapYear

--- app/calculator.js
+++ app/calculator.js
@@ -38,6 +38,16 @@ const isPalindrome = (str) => {
   return str === str.split('').reverse().join('');
 };
 
+const isLeapYear = (year) => {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return true;
+      }
+    }
+  }
+};
+
 module.exports = {
   add,
   subtract,
